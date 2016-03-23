3815cb19f7cc8798f6e503f3248338609c73b7e1
rohitgabriel <rohit.gabriel@gmail.com>
2016-03-23 13:12:29 +1300

    working WAS

--- cookbooks/WebSphereAS85/recipes/createAppServer1.rb
+++ cookbooks/WebSphereAS85/recipes/createAppServer1.rb
@@ -1,4 +1,9 @@
+Chef::Log.info("AppServer Path is #{node['WebSphereAS85']['was85-installpath']}/profiles/#{node['WebSphereAS85']['was85-appsrvname']} ")
+
 execute 'createAppSrv' do
+  path = "#{node['WebSphereAS85']['was85-installpath']}/profiles/#{node['WebSphereAS85']['was85-appsrvname']}"
+  path = path.strip
+  not_if do FileTest.directory?(path) end
   command "#{node['WebSphereAS85']['was85-binpath']}/manageprofiles.sh -create -profileName #{node['WebSphereAS85']['was85-appsrvname']} -profilePath #{node['WebSphereAS85']['was85-installpath']}/profiles/#{node['WebSphereAS85']['was85-appsrvname']} -templatePath #{node['WebSphereAS85']['was85-installpath']}/profileTemplates/managed -nodeName #{node["hostname"]}Node -cellName tempCell -hostname #{node["hostname"]}"
   cwd "#{node['WebSphereAS85']['was85-binpath']}"
   action :run
@@ -14,6 +19,9 @@ end
 
 
 execute 'addNode' do
+  path = "#{node['WebSphereAS85']['was85-installpath']}/profiles/#{node['WebSphereAS85']['was85-appsrvname']}"
+  path = path.strip
+  not_if do FileTest.directory?(path) end
   command "#{node['WebSphereAS85']['was85-installpath']}/profiles/#{node['WebSphereAS85']['was85-appsrvname']}/bin/addNode.sh #{node["hostname"]} 8879"
   cwd "#{node['WebSphereAS85']['was85-installpath']}/profiles/#{node['WebSphereAS85']['was85-appsrvname']}/bin"
   action :run
